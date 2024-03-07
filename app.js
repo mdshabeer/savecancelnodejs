e465eb740f71350e17bf997ed7f2a9d9f95fe866
mdshabeer <58755967+mdshabeer@users.noreply.github.com>
2024-03-06 20:49:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var axios = require('axios');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var predictRouter = require('./routes/predict');
 
 var app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/score', function(req, res, next) {
-    var data = req.body;
-    var url = "http://172.170.62.22:80/api/v1/service/savecancelep/score";
-    var apiKey = "Kud4KD2OX4nSGBgEAuEZaBAhc0OhUvFU";
-    var headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + apiKey
-    };
-    axios.post(url, data, { headers })
-        .then(response => {
-            res.send(response.data);
-        }).catch(error => {
-            console.error('Error:', error.message);
-            res.status(500).json(error);
-        });
-});
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/predict', predictRouter);
 
-app.listen(3000, function() {
-    console.log('App listening on port 3000');
-});
+module.exports = app;
